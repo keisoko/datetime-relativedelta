15f29b3c13c40c64d52eb146fb87d5928b268af6
Dmitriy Goldner <keisoko@gmail.com>
2022-03-02 13:21:34 -0500

    Refactoring code

--- dataclass_with_datetime_and_relativedelta.py
+++ dataclass_with_datetime_and_relativedelta.py
@@ -13,19 +13,16 @@ class AboutMe:
     place_of_residence: str
     interests: List[str] = field(default_factory=list)
 
-    @property
-    def age(self, BIRTH_YEAR=datetime(year=1969, month=9, day=5)):
+    @staticmethod
+    def age(BIRTH_YEAR=datetime(year=1969, month=9, day=5)):
         """Returns my age in years, month, days"""
-        self.current_year = datetime.now()
-        diff = relativedelta(self.current_year, BIRTH_YEAR)
+        current_year = datetime.now()
+        diff = relativedelta(current_year, BIRTH_YEAR)
         return f"{diff.years} years, {diff.months} months, {diff.days} days"
 
     def add_interest(self, new_interest):
         if new_interest not in self.interests:
             self.interests.append(new_interest)
-
-    def favorite_quote(self, quote):
-        return quote
 
 
 about_me = {
@@ -43,7 +40,7 @@ dimaG = AboutMe(**about_me["dimaG"])
 dimaG.add_interest("Gaming")
 
 print(
-    f"My name is {dimaG.name}, I was born {dimaG.age} ago in {dimaG.born_in} on {dimaG.born_on:%A, %B %d, %Y},\nI am currently living in {dimaG.place_of_residence}, and my interests are:", end="\n\n")
+    f"My name is {dimaG.name}, I was born {dimaG.age()} ago in {dimaG.born_in} on {dimaG.born_on:%A, %B %d, %Y},\nI am currently living in {dimaG.place_of_residence}, and my interests are:", end="\n\n")
 
 pprint(dimaG.interests, indent=4, width=60)
 
@@ -53,4 +50,9 @@ print("The quote I like is:\n------------------")
 
 book_excerpt = "Yes, man is mortal, but that would be only half the trouble. The worst of it is that he's sometimes unexpectedly mortal.\nThere's the trick! From The Master and Margarita by Mikhail Bulgakov."
 
-print(dimaG.favorite_quote(f"\"{book_excerpt}\""))
+
+def favorite_quote(quote):
+    return quote
+
+
+print(favorite_quote(f"\"{book_excerpt}\""))
