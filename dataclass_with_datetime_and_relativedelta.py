dd7f895281179021aab2423406ed3afe43e77a48
Dmitriy Goldner <keisoko@gmail.com>
2022-08-30 17:14:08 -0400

    Refactor code for clarity.

--- dataclass_with_datetime_and_relativedelta.py
+++ dataclass_with_datetime_and_relativedelta.py
@@ -1,37 +1,47 @@
 from dataclasses import dataclass, field
 from datetime import datetime
 from pprint import pprint
-from typing import List
 
 from dateutil.relativedelta import relativedelta
+
+
+@dataclass(frozen=True)
+class ConstantNamespace:
+    BIRTH_YEAR = datetime(year=1969, month=9, day=5)
+
+
+constant = ConstantNamespace()
 
 
 @dataclass
 class AboutMe:
     name: str
     born_in: str
-    born_on: datetime
     place_of_residence: str
-    interests: List[str | dict] = field(default_factory=list)
+    interests: list[str | dict] = field(default_factory=list)
 
     @staticmethod
-    def age(BIRTH_YEAR=datetime(year=1969, month=9, day=5)):
+    def age():
         """Returns my age in years, month, days"""
         current_year = datetime.now()
-        diff = relativedelta(current_year, BIRTH_YEAR)
+        diff = relativedelta(current_year, constant.BIRTH_YEAR)
         return f"{diff.years} years, {diff.months} months, {diff.days} days"
 
     def add_interest(self, new_interest):
         if new_interest not in self.interests:
             self.interests.append(new_interest)
 
+    @property
+    def say_description(self) -> str:
+        """Returns my info"""
+        return f"My name is {self.name}, I am {self.age()} old from {self.born_in}."
 
-def main():
 
-    dimaG = AboutMe(
-        name="Dmitriy G",
+def execute_main():
+
+    dmitriy = AboutMe(
+        name="Dmitriy G.",
         born_in="Kiev, Ukraine",
-        born_on=datetime(year=1969, month=9, day=5),
         place_of_residence="Roselle, New Jersey",
         interests=[
             {"Learning Programming": ["Python", "HTML", "CSS", "JavaScript"]},
@@ -40,15 +50,12 @@ def main():
         ],
     )
 
-    dimaG.add_interest("Gaming")
+    dmitriy.add_interest("Gaming")
 
-    print(
-        f"My name is {dimaG.name}, I was born {dimaG.age()} years ago in {dimaG.born_in} on\n{dimaG.born_on:%A, %B %d, %Y}, I am currently living in {dimaG.place_of_residence},\nand my interests are:",
-        end="\n\n",
-    )
+    print(dmitriy.say_description, "\n")
 
-    pprint(dimaG.interests, indent=5)
+    pprint(dmitriy)
 
 
 if __name__ == "__main__":
-    main()
+    execute_main()
