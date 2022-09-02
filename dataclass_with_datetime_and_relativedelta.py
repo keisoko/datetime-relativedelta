87b0f7a8f903e7ebf69ecd52b0552f4e489e72b8
Dmitriy Goldner <keisoko@gmail.com>
2022-09-02 17:22:34 -0400

    Refactor code. Replace pprint import with pformat Add born_on field. Remove place_of_residence. Implement pformat method.

--- dataclass_with_datetime_and_relativedelta.py
+++ dataclass_with_datetime_and_relativedelta.py
@@ -1,12 +1,14 @@
 from dataclasses import dataclass, field
 from datetime import datetime
-from pprint import pprint
+from pprint import pformat
 
 from dateutil.relativedelta import relativedelta
 
 
 @dataclass(frozen=True)
 class ConstantNamespace:
+    """Class for storing constant namespaces"""
+
     BIRTH_YEAR = datetime(year=1969, month=9, day=5)
 
 
@@ -17,24 +19,25 @@ constant = ConstantNamespace()
 class AboutMe:
     name: str
     born_in: str
-    place_of_residence: str
+    born_on: datetime
     interests: list[str | dict] = field(default_factory=list)
 
     @staticmethod
-    def age():
+    def age() -> str:
         """Returns my age in years, month, days"""
         current_year = datetime.now()
         diff = relativedelta(current_year, constant.BIRTH_YEAR)
         return f"{diff.years} years, {diff.months} months, {diff.days} days"
 
-    def add_interest(self, new_interest):
+    def add_interest(self, new_interest: str) -> None:
+        """Adds new interest to the interests list"""
         if new_interest not in self.interests:
             self.interests.append(new_interest)
 
     @property
     def say_description(self) -> str:
         """Returns my info"""
-        return f"My name is {self.name}, I am {self.age()} old from {self.born_in}."
+        return f"My name is {self.name}, I am {self.age()} old from {self.born_in}. I was born on {self.born_on: %A, %B %d, %Y}."
 
 
 def execute_main():
@@ -42,7 +45,7 @@ def execute_main():
     dmitriy = AboutMe(
         name="Dmitriy G.",
         born_in="Kiev, Ukraine",
-        place_of_residence="Roselle, New Jersey",
+        born_on=datetime(year=1969, month=9, day=5),
         interests=[
             {"Learning Programming": ["Python", "HTML", "CSS", "JavaScript"]},
             "Science Fiction & Fantasy Audiobooks",
@@ -54,7 +57,15 @@ def execute_main():
 
     print(dmitriy.say_description, "\n")
 
-    pprint(dmitriy)
+    # Removes single quotes and curly brackets from class
+    # object output by using pprint module's pformat
+
+    about_me = pformat(dmitriy)
+    char_to_remove = ["{", "}", "'"]
+    for char in char_to_remove:
+        about_me = about_me.replace(char, "")
+
+    print(about_me)
 
 
 if __name__ == "__main__":
