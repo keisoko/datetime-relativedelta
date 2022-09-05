8d4065abed052a92832a5651b467281a14c65cbf
Dmitriy Goldner <keisoko@gmail.com>
2022-09-05 16:08:06 -0400

    Add files via upload
    Add source reference module for local function import

---
+++ my_python_modules.py
@@ -0,0 +1,24 @@
+"""My python modules"""
+
+import random
+import string
+from pprint import pformat
+from typing import Any, Callable
+
+
+def operate(func: Callable, item: Any) -> Callable:
+    """Executes the higher-order function"""
+    return func(item)
+
+
+def generate_id(length: int) -> str:
+    """Helper function to generate id."""
+    return "".join(random.choices(string.hexdigits.upper(), k=length))
+
+
+def pretty_print_item(item_to_pformat: Any, char_to_remove: Any) -> str:
+    """Returns a string pretty formatted with pformat"""
+    formatted_item = pformat(item_to_pformat)
+    for char in char_to_remove:
+        formatted_item = formatted_item.replace(char, "")
+    return formatted_item
